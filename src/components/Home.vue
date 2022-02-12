eddb22b1b2597a30daa54b85186311da2c7e8c3a
Denis Nikita <denisnikita2027@yandex.com>
2022-02-12 09:04:20 -0800

    vue-typescript

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    Home
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import UserService from "@/services/UserService";
+
+@Component
+export default class Home extends Vue {
+  private content = "";
+
+  mounted() {
+    UserService.getPublicContent().then(
+      (response) => {
+        this.content = response.data;
+      },
+      (error) => {
+        this.content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+      }
+    );
+  }
+}
+</script>
